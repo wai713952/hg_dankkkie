790735d7680e83095dd6cccc7b995224d64f7567
Jack_Lewise <wai.test@mail.kmutt.ac.th>
2020-06-11 19:59:16 +0700

    revolver system + model + animation

---
+++ project test/Assets/revolver/RevolverDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevolverDamage : MonoBehaviour
+{
+[SerializeField] float enemyhp = 6;
+[SerializeField] public float enemyhitforceback;
+[SerializeField] public float enemyhitforceup;
+void OnTriggerStay(Collider bullet) 
+    {
+        if(bullet.gameObject.name == "bulletline")
+            {
+                if(Input.GetKeyDown(KeyCode.Mouse0) && Revolvershoot.ammo > 0)
+                {
+                    GetComponent<Rigidbody>().AddForce(-transform.forward * enemyhitforceback);
+                    GetComponent<Rigidbody>().AddForce(transform.up * enemyhitforceup);
+
+                    enemyhp--;
+                    if(enemyhp <= 0)
+                        {
+                            Destroy(gameObject);
+                        }
+                    Debug.Log(enemyhp);
+                }
+                
+            }
+    }
+}
---
+++ project test/Assets/revolver/Revolvershoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolvershoot : MonoBehaviour
+{
+private Animator Gun;
+public static float ammo = 6;
+
+    void Start()
+    {
+        Gun = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)&& ammo > 0)
+        {
+            Gun.SetBool("shoot",true);
+            ammo--;
+        }
+        if(Input.GetKeyUp(KeyCode.Mouse0)&& ammo > 0)
+        {
+            Gun.SetBool("shoot",false);
+        }
+    }
+}
